b7d7aa67385d79eb87cad156be8aafb355fe8172
Yuriy Bevov <jury19y@mail.ru>
2022-05-30 21:31:58 +0300

    parallax

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,9 @@
 //import './modules/animation.js';
 //import './modules/anim2.js'
 import './modules/anim3.js'
+import Parallax from 'parallax-js'
 
+setTimeout(() => {
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene);
+}, 2300);
