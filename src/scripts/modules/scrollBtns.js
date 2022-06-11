14c022de1ab720481a75dbe09f9b26d22923cff4
Yuriy Bevov <jury19y@mail.ru>
2022-06-11 14:36:46 +0300

    portfolio webp

--- src/scripts/modules/scrollBtns.js
+++ src/scripts/modules/scrollBtns.js
@@ -1,12 +1,16 @@
-import { scrollIntoView } from "seamless-scroll-polyfill";
+import { scrollBy } from "seamless-scroll-polyfill";
 const scrollBtns = document.querySelectorAll('.anchor-link');
+
+function getCoords(elem) {
+  return elem.getBoundingClientRect().top - 125;
+}
 
 const onClickScrollToSection = (evt) => {
   evt.preventDefault();
   let anchor = evt.currentTarget.getAttribute('data-scroll-to');
   let target = document.querySelector('#' + anchor);
 
-  scrollIntoView(target, { behavior: "smooth", block: "start"});
+  scrollBy(window, { behavior: "smooth", top: getCoords(target) });
 }
 
 scrollBtns.forEach(btn => {
